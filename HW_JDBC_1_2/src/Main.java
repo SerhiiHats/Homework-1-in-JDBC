a4e10d741f2f7bef90d405b819227a382597a7a5
Serhi Hats <gatsserv@gmail.com>
2022-08-25 02:17:25 +0300

    Add second commit in homework 1 at JDBC

--- HW_JDBC_1_2/src/Main.java
+++ HW_JDBC_1_2/src/Main.java
@@ -5,15 +5,19 @@ import services.ReadRequest;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+
+/**Задание 2
+ Используя дополнительное задания все вопросы записать в текстовом файле с новой строки каждый
+ и используя потоки ввода-вывода считать с файла все запросы и выполнить.
+ Текстовый файл в корневой парке проекта: src/requestTestOptSklad.txt
+ **/
 
 public class Main {
     public static void main(String[] args) {
         int count = 1;
         try(Connection connection = DBConnect.getConnection()) {
-            List<String> listRequest = new ArrayList<>();
-            listRequest = new ReadRequest().getListRequest();
+            List<String> listRequest = new ReadRequest().getListRequest();
             UserRepo userRepo = new UserRepo(connection);
 
             for (String sqlRequest : listRequest){
